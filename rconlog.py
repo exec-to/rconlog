bd1aeaa51a8db309e7e36f342d87bc7de0926a6b
Sokolov Roman <sokolov-at@yandex.ru>
2020-02-15 12:04:05 +0300

    Initial commit

---
+++ rconlog.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+# import module_name
+
+## specification
+
+# rcon_monitor - вызывает rcon_client, обновляет БД
+
+# rcon_client - клиент rcon, получает данные от одного игрового сервера
+
+# rcon_control - flask-админка для управления
+
+# rcon_ipset - модуль для управления таблицами ipset
+
+# rcon_iptables - модуль для управления правилами iptables
+
+# rcon_firewall - модуль для управления фаерволлом
+
+# rcon_config - конфигурация наблюдаемых игровых серверов
+
+# rconlog_config - конфигурация приложения (БД, фаерволлы и т.п.)
+
+# rcon_log - модуль логгирования
+
