e75ab1782fa797f85621fed75ee158a4357d737f
Sokolov Roman <sokolov-at@yandex.ru>
2020-02-15 14:30:12 +0300

    Parse arguments

--- rconlog.py
+++ rconlog.py
@@ -1,25 +1,40 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: UTF-8 -*-
+import argparse
+import textwrap
 
-# import module_name
+#                          RCON-based logger application
+#                          -----------------------------
+#                          server - run program on monitor and web server
+#                          monitor - run program on logger server
+#                          firewall - run program on firewall server
+#                          web - run web-based admin panel
+#                          stat - print logger statistics
+#                          cli - enter cli mode
 
-## specification
 
+
+def create_parser():
+    _parser = argparse.ArgumentParser(description='RCON-based logger application')
+    _parser.add_argument('--mode', dest='mode', action='store',  nargs=1, required=True, default='stat',
+                         choices={'server', 'monitor', 'firewall', 'web', 'stat', 'cli'},
+                         help='Run app in selected mode')
+    return _parser
+
+
+if __name__ == '__main__':
+    parser = create_parser()
+    namespace = parser.parse_args()
+
+# specification
 # rcon_monitor - вызывает rcon_client, обновляет БД
-
+# rcon_stat - модуль выводит статистику по своей работе
 # rcon_client - клиент rcon, получает данные от одного игрового сервера
-
 # rcon_control - flask-админка для управления
-
 # rcon_ipset - модуль для управления таблицами ipset
-
 # rcon_iptables - модуль для управления правилами iptables
-
 # rcon_firewall - модуль для управления фаерволлом
-
 # rcon_config - конфигурация наблюдаемых игровых серверов
-
 # rconlog_config - конфигурация приложения (БД, фаерволлы и т.п.)
-
 # rcon_log - модуль логгирования
 
---
+++ rconlog_config.example.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+database = dict(
+    host='localhost',
+    port=3306,
+    db='rcondb',
+    user='user',
+    passwd='pass'
+
+)
